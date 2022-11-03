a9a77bc7b7876113e4fbd938919cd9f457fc17e2
thisisrick25 <swapnaneel06@gmail.com>
2022-11-04 03:20:51 +0530

    add sky and ground

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { Physics } from "@react-three/cannon";
+import { Sky } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Ground } from "./components/Ground";
 
 function App() {
   return (
     <>
-      <div>outside canvas</div>
-      <Canvas></Canvas>
+      <Canvas>
+        <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5} />
+        <Physics>
+          <Ground />
+        </Physics>
+      </Canvas>
     </>
   );
 }
