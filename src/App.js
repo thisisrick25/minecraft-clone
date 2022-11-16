1c53282e0d7858846db9cf50b7026714eb384973
thisisrick25 <swapnaneel06@gmail.com>
2022-11-16 23:25:43 +0530

    add fpv camera and crosshair

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Physics } from "@react-three/cannon";
 import { Sky } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Fpv } from "./components/Fpv";
 import { Ground } from "./components/Ground";
 import { Player } from "./components/Player";
 
@@ -10,11 +11,13 @@ function App() {
       <Canvas>
         <Sky sunPosition={[100, 100, 20]} />
         <ambientLight intensity={0.5} />
+        <Fpv/>
         <Physics>
           <Player />
           <Ground />
         </Physics>
       </Canvas>
+      <div className="absolute center crosshair">+</div>
     </>
   );
 }
