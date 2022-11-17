4ffea0058d873e6b6ed236b1db25583e3967b216
thisisrick25 <swapnaneel06@gmail.com>
2022-11-17 21:04:50 +0530

    add cubes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Physics } from "@react-three/cannon";
 import { Sky } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Cubes } from "./components/Cubes";
 import { Fpv } from "./components/Fpv";
 import { Ground } from "./components/Ground";
 import { Player } from "./components/Player";
@@ -11,9 +12,10 @@ function App() {
       <Canvas>
         <Sky sunPosition={[100, 100, 20]} />
         <ambientLight intensity={0.5} />
-        <Fpv/>
+        <Fpv />
         <Physics>
           <Player />
+          <Cubes />
           <Ground />
         </Physics>
       </Canvas>
